760a8cc7eba2f91f2f2d8477eb754480eb4ac52d
Mariana Victorino <victorino.marii@gmail.com>
2024-10-14 14:08:45 +0100

    add styles and map items

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let score = 0;
 const grid = document.querySelector(".grid");
 
 
+
 // 0 - food | 1 - wall | 2 - enemies cave | 3 - superfood | 4 - empty 
 const gridLayout = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -40,10 +41,29 @@ const gridLayout = [
 
 //creating the game space
 
+const squares = []; // each square (div) created needs to go to this new array and this will be styled acording to the nature of element from the gridLayout
+
 function createGameSpace() {
     for (i = 0; i < gridLayout.length; i++) {
         const square = document.createElement("div");
         grid.appendChild(square);
+        squares.push(square)
+
+        if (gridLayout[i] === 0) {
+            squares[i].classList.add("food");
+        }
+        else if (gridLayout[i] === 1) {
+            squares[i].classList.add("wall");
+        }
+        else if (gridLayout[i] === 2) {
+            squares[i].classList.add("enemies-cave");
+        }
+        else if (gridLayout[i] === 3) {
+            squares[i].classList.add("superfood");
+        }
+        else if (gridLayout[i] === 4) {
+            squares[i].classList.add("empty");
+        }
     }
 }
 createGameSpace();
