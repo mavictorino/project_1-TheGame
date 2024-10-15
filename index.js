3f06d6df6aafd453253e3f2a164a1203c39a7004
Mariana Victorino <victorino.marii@gmail.com>
2024-10-15 13:13:08 +0100

    add pacman moviments and points

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-const pointsDisplay = document.getElementById("score")
+const pointsDisplay = document.getElementById("points");
+let points = 0;
 const width = 28;
-let score = 0;
 const grid = document.querySelector(".grid");
 
 
@@ -46,6 +46,7 @@ const squares = []; // each square (div) created needs to go to this new array a
 function createGameSpace() {
     for (i = 0; i < gridLayout.length; i++) {
         const square = document.createElement("div");
+        square.id = i;
         grid.appendChild(square);
         squares.push(square)
 
@@ -68,9 +69,64 @@ function createGameSpace() {
 }
 createGameSpace();
 
-// creating pacman: - need to determine the div index to position pacman in the middle // index = (row * column) + column 
+// creating pacman: - need to determine the square.id to position pacman in the middle 
 
-let pacmanCurrentIndex = 490;
+let pacmanCurrentIndex = 489;
 squares[pacmanCurrentIndex].classList.add("pacman");
 
+//moving pacman
+
+const directions = {
+    "ArrowLeft": -1,
+    "ArrowRight": +1,
+    //in a 2d array the vertical movement will be defined by the length
+    "ArrowUp": -width, 
+    "ArrowDown": width,
+};
+
+function movePacman(e) {
+    const direction = directions[e.key];
+    if (direction === undefined) return;
+
+    const nextIndex = pacmanCurrentIndex + direction;
+
+    //check wall collision
+    if (!squares[nextIndex].classList.contains("wall")) {
+        squares[pacmanCurrentIndex].classList.remove("pacman");
+        
+        pacmanCurrentIndex = nextIndex;
+
+        //pass tunnels
+        if (pacmanCurrentIndex === 364) {
+            pacmanCurrentIndex = 391; 
+        } else if (pacmanCurrentIndex === 391) {
+            pacmanCurrentIndex = 364; 
+        }
+        squares[pacmanCurrentIndex].classList.add("pacman");
+    }
+    eatFood();
+    eatSuperfood();
+};
+
+document.addEventListener('keydown', movePacman);
+
+// pacman eating food
+
+function eatFood() {
+    if (squares[pacmanCurrentIndex].classList.contains("food")) {
+        points++;
+        pointsDisplay.innerHTML = points;
+        squares[pacmanCurrentIndex].classList.remove("food")
+    }
+};
+
+// pacman eating superfood
+
+function eatSuperfood() {
+    if(squares[pacmanCurrentIndex].classList.contains("superfood")) {
+        points += 20;
+        pointsDisplay.innerHTML = points;
+        squares[pacmanCurrentIndex].classList.remove("superfood")
+    }
+};
 
